c22025e9210c167e973e9257f8a24dce963d9506
Minghao Li <alvissreimu@gmail.com>
2018-03-31 23:07:09 -0700

    change to unit test

--- src/test/java/com/alviss/hadoop/UserAgentTest.java
+++ src/test/java/com/alviss/hadoop/UserAgentTest.java
@@ -2,10 +2,12 @@ package com.alviss.hadoop;
 
 import com.kumkee.userAgent.UserAgent;
 import com.kumkee.userAgent.UserAgentParser;
+import org.junit.Test;
 
 public class UserAgentTest {
 
-    public static void main(String[] args) {
+    @Test
+    public void testUserAgentParser() {
         String source = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Win64; x64; Trident/4.0; .NET CLR 2.0.50727; SLCC2; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C)";
         UserAgentParser userAgentParser = new UserAgentParser();
         UserAgent userAgent = userAgentParser.parse(source);
